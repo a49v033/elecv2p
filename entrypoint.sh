73a813bb5e954292355c902246927db5c2fabf8b
小朋 <2810580330@qq.com>
2021-03-14 13:17:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-if [ ! -e '/usr/local/app/script/Shell/scripts/jdCookie.js' ]; then
-    cp -r /tmp/Shell/scripts /usr/local/app/script/Shell
-    echo "拷贝仓库代码"
-fi
-if [ ! -e '/usr/local/app/script/JSFile/0body.js' ]; then
-    cp -r /tmp/JSFile /usr/local/app/script
-    echo "拷贝JSFile代码"
-fi
-
-if [ ! -e '/usr/local/app/script/Lists/task.list' ]; then
-    cp -r /tmp/Lists /usr/local/app/script
-    echo "拷贝Lists代码"
-fi
+echo "开始远程下载elecV2P仓库代码"
+git clone https://github.com/elecV2/elecV2P.git /usr/local/app
+sed -i "s/60000/0/g" /usr/local/app/func/exec.js
+rm -r /usr/local/app/package.json
+mv /tmp/package.json /usr/local/app/package.json
+echo "开始安装npm依赖"
+cd /usr/local/app && npm install
 cd /usr/local/app && pm2 start index.js --name elecV2P
 tail -f /dev/null
