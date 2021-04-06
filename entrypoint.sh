69127f879986ad694fe3d4b241fee4e5fd238dbb
小朋 <2810580330@qq.com>
2021-04-07 03:11:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ ! -e '/usr/local/app/script/Shell/scripts/jdCookie.js' ]; then
-    cp -r /tmp/Shell/scripts /usr/local/app/script/Shell
-    echo "拷贝仓库代码"
-fi
 if [ ! -e '/usr/local/app/script/JSFile/0body.js' ]; then
     cp -r /tmp/JSFile /usr/local/app/script
     echo "拷贝JSFile代码"
